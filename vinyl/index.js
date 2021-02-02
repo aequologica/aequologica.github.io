8a59aeb38da844509c4d3a542ba204e6cce994e1
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-02-02 23:43:22 +0100

    plus solide

--- vinyl/index.js
+++ vinyl/index.js
@@ -2,291 +2,295 @@
 
 import { fadeIn, fadeOut } from "./fadeInFadeOut.js";
 
-// compile the templates
-const templates = {
-  song: undefined,
-  video: undefined,
-  sound: undefined,
-  markdown: undefined,
-  url: undefined,
-};
-
-Object.keys(templates).map(function (key, index) {
-  templates[key] = Handlebars.compile(
-    document.getElementById(key + "-template").innerHTML
-  );
-});
-
-const urlParams = new URLSearchParams(window.location.search);
-const no_recursion = urlParams.get("no_recursion") || false;
-const file = urlParams.get("data") || "index.json";
-
-// http://stackoverflow.com/questions/20789373/shuffle-array-in-ng-repeat-angular
-// -> Fisher–Yates shuffle algorithm
-function shuffleArray(array) {
-  var m = array.length,
-    t,
-    i;
-  // While there remain elements to shuffle
-  while (m) {
-    // Pick a remaining element…
-    i = Math.floor(Math.random() * m--);
-    // And swap it with the current element.
-    t = array[m];
-    array[m] = array[i];
-    array[i] = t;
-  }
-  return array;
-}
-
 $(document).ready(function () {
-  let $thePlayer = undefined;
-  let $theButton = undefined;
+  // and remember the jqxhr object for this request
+  $.get("body.html").done(function (body) {
 
-  let theBackground = {
-    start: () => {},
-    stop: () => {},
-    toggle: () => {},
-  };
+    $('body').append(body);
 
-  const backgroundClass = (background, background_animated) => {
-    const start = () => {
-      $start_pause_button.children("img").attr("src", "./svgs/pause.svg");
-      // ./svgs/vinyl_rotating.svg
-      $("html").css({
-        background: "url(" + background_animated + ") no-repeat center center",
-        "background-size": "contain",
-      });
-      return "started";
+    const templates = {
+      // compile the templates
+      song: undefined,
+      video: undefined,
+      sound: undefined,
+      markdown: undefined,
+      url: undefined,
     };
-    const stop = () => {
-      $start_pause_button.children("img").attr("src", "./svgs/play.svg");
-      // ./svgs/vinyl.svg
-      $("html").css({
-        background: "url(" + background + ") no-repeat center center",
-        "background-size": "contain",
-      });
-      return "stopped";
-    };
-    const toggle = () => {
-      if (
-        $start_pause_button.children("img").attr("src") == "./svgs/pause.svg"
-      ) {
-        return stop();
-      } else {
-        return start();
-      }
-    };
-    return {
-      start: start,
-      stop: stop,
-      toggle: toggle,
-    };
-  };
 
-  //  destroy if exists
-  function destroyiframeIfExists($button) {
-    if ($thePlayer) {
-      $thePlayer.remove();
-      $thePlayer = undefined;
-    }
-    if ($theButton) {
-      $theButton.html($theButton.data("text"));
-      if (!$button || $theButton.prop("id") === $button.prop("id")) {
-        $theButton = undefined;
-        return undefined;
-      }
-    }
-    if ($button) {
-      $button.html("<span style='color:lightblue'>close ×</span>");
-    }
-    return $button;
-  }
-
-  function getStyleAndParentCard($button) {
-    const $parentCard = $button.parents("div.card");
-    const $img = $parentCard.children("img");
-    const pos = $img.position();
-    const oh = $img.outerHeight();
-    const ow = $img.outerWidth();
-
-    return {
-      style: {
-        position: "absolute",
-        top: pos.top,
-        left: pos.left,
-        width: ow + "px",
-        height: oh + "px",
-        "background-color": "#212529",
-      },
-      $parentCard: $parentCard,
-    };
-  }
-
-  function showiframe(e, template) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    // destroy
-    theBackground.stop();
-    $theButton = destroyiframeIfExists($(e.currentTarget));
-
-    if ($theButton) {
-      // create
-      if ($theButton.attr("class").indexOf("sound") != -1) {
-        theBackground.start();
-      }
-      const styleAndParentCard = getStyleAndParentCard($theButton);
-      const iframe = template({
-        id: $theButton.data("id"),
-      });
-      $thePlayer = $(iframe);
-      styleAndParentCard.$parentCard.append(
-        $thePlayer.css(styleAndParentCard.style)
+    Object.keys(templates).map(function (key, index) {
+      templates[key] = Handlebars.compile(
+        document.getElementById(key + "-template").innerHTML
       );
-    }
-  }
+    });
 
-  function addEventHandlers() {
-    $("div.card img.img-fluid").on("click", (e) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const no_recursion = urlParams.get("no_recursion") || false;
+    const file = $("head meta[name='file']").attr('content') || "index.json";
+
+    // http://stackoverflow.com/questions/20789373/shuffle-array-in-ng-repeat-angular
+    // -> Fisher–Yates shuffle algorithm
+    function shuffleArray(array) {
+      var m = array.length,
+        t,
+        i;
+      // While there remain elements to shuffle
+      while (m) {
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+      }
+      return array;
+    }
+
+    let $thePlayer = undefined;
+    let $theButton = undefined;
+
+    let theBackground = {
+      start: () => {},
+      stop: () => {},
+      toggle: () => {},
+    };
+
+    const backgroundClass = (background, background_animated) => {
+      const start = () => {
+        $start_pause_button.children("img").attr("src", "./svgs/pause.svg");
+        // ./svgs/vinyl_rotating.svg
+        $("html").css({
+          background:
+            "url(" + background_animated + ") no-repeat center center",
+          "background-size": "contain",
+        });
+        return "started";
+      };
+      const stop = () => {
+        $start_pause_button.children("img").attr("src", "./svgs/play.svg");
+        // ./svgs/vinyl.svg
+        $("html").css({
+          background: "url(" + background + ") no-repeat center center",
+          "background-size": "contain",
+        });
+        return "stopped";
+      };
+      const toggle = () => {
+        if (
+          $start_pause_button.children("img").attr("src") == "./svgs/pause.svg"
+        ) {
+          return stop();
+        } else {
+          return start();
+        }
+      };
+      return {
+        start: start,
+        stop: stop,
+        toggle: toggle,
+      };
+    };
+
+    //  destroy if exists
+    function destroyiframeIfExists($button) {
+      if ($thePlayer) {
+        $thePlayer.remove();
+        $thePlayer = undefined;
+      }
+      if ($theButton) {
+        $theButton.html($theButton.data("text"));
+        if (!$button || $theButton.prop("id") === $button.prop("id")) {
+          $theButton = undefined;
+          return undefined;
+        }
+      }
+      if ($button) {
+        $button.html("<span style='color:lightblue'>close ×</span>");
+      }
+      return $button;
+    }
+
+    function getStyleAndParentCard($button) {
+      const $parentCard = $button.parents("div.card");
+      const $img = $parentCard.children("img");
+      const pos = $img.position();
+      const oh = $img.outerHeight();
+      const ow = $img.outerWidth();
+
+      return {
+        style: {
+          position: "absolute",
+          top: pos.top,
+          left: pos.left,
+          width: ow + "px",
+          height: oh + "px",
+          "background-color": "#212529",
+        },
+        $parentCard: $parentCard,
+      };
+    }
+
+    function showiframe(e, template) {
       e.preventDefault();
       e.stopPropagation();
-      $(e.currentTarget)
-        .parent()
-        .find(".btn-group button:first-child")
-        .trigger("click");
-    });
 
-    $("div.card button.vid").on("click", (e) => showiframe(e, templates.video));
-    $("div.card button.sound").on("click", (e) =>
-      showiframe(e, templates.sound)
-    );
-    $("div.card button.markdown").on("click", (e) =>
-      showiframe(e, templates.markdown)
-    );
-    $("div.card button.url").on("click", (e) => showiframe(e, templates.url));
-  }
+      // destroy
+      theBackground.stop();
+      $theButton = destroyiframeIfExists($(e.currentTarget));
 
-  function insertCards(datums, $parent) {
-    const shuffle = shuffleArray([...datums.order]);
-
-    shuffle.forEach((key) => {
-      $parent.append(
-        $(
-          templates.song({
-            key: key,
-            val: datums.songs[key],
-            covers: datums.covers || "covers",
-            cover: () => {
-              if (datums.songs[key].extension) {
-                return key + datums.songs[key].extension;
-              }
-              if (key == "brouillard") {
-                const random_boolean = Math.random() < 0.5;
-                if (random_boolean) {
-                  return "psycho_I.jpg";
-                } else {
-                  return "psycho_II.jpg";
-                }
-              }
-              return key + ".jpg";
-            },
-          })
-        )
-      );
-    });
-
-    addEventHandlers();
-  }
-
-  const $start_pause_button = $("#start_pause_button");
-  $start_pause_button.on("click", (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if ("stopped" == theBackground.toggle()) {
-      /* destroyiframeIfExists(); */
-    }
-  });
-
-  // Assign handlers immediately after making the request,
-  // and remember the jqxhr object for this request
-  const jqxhr = $.get(file).done(function (data) {
-    if (no_recursion && data.no_recursion) {
-      const index = data.order.indexOf(data.no_recursion);
-      if (index > -1) {
-        data.order.splice(index, 1);
+      if ($theButton) {
+        // create
+        if ($theButton.attr("class").indexOf("sound") != -1) {
+          theBackground.start();
+        }
+        const styleAndParentCard = getStyleAndParentCard($theButton);
+        const iframe = template({
+          id: $theButton.data("id"),
+        });
+        $thePlayer = $(iframe);
+        styleAndParentCard.$parentCard.append(
+          $thePlayer.css(styleAndParentCard.style)
+        );
       }
     }
 
-    if (data.title) {
-      $("title").text(data.title);
+    function addEventHandlers() {
+      $("div.card img.img-fluid").on("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        $(e.currentTarget)
+          .parent()
+          .find(".btn-group button:first-child")
+          .trigger("click");
+      });
+
+      $("div.card button.vid").on("click", (e) =>
+        showiframe(e, templates.video)
+      );
+      $("div.card button.sound").on("click", (e) =>
+        showiframe(e, templates.sound)
+      );
+      $("div.card button.markdown").on("click", (e) =>
+        showiframe(e, templates.markdown)
+      );
+      $("div.card button.url").on("click", (e) => showiframe(e, templates.url));
+    }
+
+    function insertCards(datums, $parent) {
+      const shuffle = shuffleArray([...datums.order]);
+
+      shuffle.forEach((key) => {
+        $parent.append(
+          $(
+            templates.song({
+              key: key,
+              val: datums.songs[key],
+              covers: datums.covers || "covers",
+              cover: () => {
+                if (datums.songs[key].extension) {
+                  return key + datums.songs[key].extension;
+                }
+                if (key == "brouillard") {
+                  const random_boolean = Math.random() < 0.5;
+                  if (random_boolean) {
+                    return "psycho_I.jpg";
+                  } else {
+                    return "psycho_II.jpg";
+                  }
+                }
+                return key + ".jpg";
+              },
+            })
+          )
+        );
+      });
+
+      addEventHandlers();
+    }
+
+    const $start_pause_button = $("#start_pause_button");
+    $start_pause_button.on("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if ("stopped" == theBackground.toggle()) {
+        /* destroyiframeIfExists(); */
+      }
+    });
+
+    $.get(file).done(function (data) {
+      if (no_recursion && data.no_recursion) {
+        const index = data.order.indexOf(data.no_recursion);
+        if (index > -1) {
+          data.order.splice(index, 1);
+        }
+      }
+
       const styleTemplate = Handlebars.compile(
         document.getElementById("style-template").innerHTML
       );
-      $("head").append(styleTemplate(data.title));
-    }
+      $("head").append(styleTemplate($("title").text()));
 
-    if (data.copyright) {
-      $("#copyright").text(data.copyright);
-    }
+      if (data.copyright) {
+        $("#copyright").text(data.copyright);
+      }
 
-    $('head meta[property="fb:app_id"]').remove();
-    $('head meta[property="og:locale"]').remove();
-    $('head meta[property="og:title"]').remove();
-    $('head meta[property="og:type"]').remove();
-    $('head meta[property="og:url"]').remove();
-    $('head meta[property="og:description"]').remove();
-    $('head meta[property="og:site_name"]').remove();
+      $('head meta[property="fb:app_id"]').remove();
+      $('head meta[property="og:locale"]').remove();
+      $('head meta[property="og:title"]').remove();
+      $('head meta[property="og:type"]').remove();
+      $('head meta[property="og:url"]').remove();
+      $('head meta[property="og:description"]').remove();
+      $('head meta[property="og:site_name"]').remove();
 
-    $('head meta[property="og:image"]').remove();
-    $('head meta[property="og:image:url"]').remove();
-    $('head meta[property="og:image:type"]').remove();
-    $('head meta[property="og:image:width"]').remove();
-    $('head meta[property="og:image:height"]').remove();
+      $('head meta[property="og:image"]').remove();
+      $('head meta[property="og:image:url"]').remove();
+      $('head meta[property="og:image:type"]').remove();
+      $('head meta[property="og:image:width"]').remove();
+      $('head meta[property="og:image:height"]').remove();
 
-    if (!data.openGraphHeader) {
-      $("#vinyl_share").remove();
-    } else {
-      $.get(data.openGraphHeader).done(function (header) {
-        $("head").append($(header));
-      });
-    }
+      if (!data.openGraphHeader) {
+        $("#vinyl_share").remove();
+      } else {
+        $.get(data.openGraphHeader).done(function (header) {
+          $("head").append($(header));
+        });
+      }
 
-    if (data.navbarheader) {
-      $.get(data.navbarheader).done(function (header) {
-        $("#navbarHeader").prepend($(header));
-      });
-    }
+      if (data.navbarheader) {
+        $.get(data.navbarheader).done(function (header) {
+          $("#navbarHeader").prepend($(header));
+        });
+      }
 
-    if (data.icon) {
-      $("#vinyl_brand img").attr("src", data.icon);
-    }
+      if (data.icon) {
+        $("#vinyl_brand img").attr("src", data.icon);
+      }
 
-    if (data.background && data.background_animated) {
-      $("html").css({
-        background: "url(" + data.background + ") no-repeat center center",
-        "background-size": "contain",
-      });
-      theBackground = backgroundClass(
-        data.background,
-        data.background_animated
-      );
-    } else {
-      $("#start_pause_button").remove();
-    }
+      if (data.background && data.background_animated) {
+        $("html").css({
+          background: "url(" + data.background + ") no-repeat center center",
+          "background-size": "contain",
+        });
+        theBackground = backgroundClass(
+          data.background,
+          data.background_animated
+        );
+      } else {
+        $("#start_pause_button").remove();
+      }
 
-    const $row = $("#songs");
-    insertCards(data, $row);
-    fadeIn($row.children(".col"), 111, () => {});
+      const $row = $("#songs");
+      insertCards(data, $row);
+      fadeIn($row.children(".col"), 111, () => {});
 
-    $("#shuffle_button").on("click", (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      fadeOut($row.children(".col"), 333, () => {
-        $row.empty();
-        insertCards(data, $row);
-        fadeIn($row.children(".col"), 666, () => {}, shuffleArray);
+      $("#shuffle_button").on("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        fadeOut($row.children(".col"), 333, () => {
+          $row.empty();
+          insertCards(data, $row);
+          fadeIn($row.children(".col"), 666, () => {}, shuffleArray);
+        });
       });
     });
   });
