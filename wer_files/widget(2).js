6f7430f3840d3c25daf8d5b69a719eea379cd11b
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2022-03-11 14:46:23 +0100

    wer

---
+++ wer_files/widget(2).js
@@ -0,0 +1,53 @@
+function heredoc(f) {return f.toString().match(/\/\*\s*([\s\S]*?)\s*\*\//m)[1];};var fw_most_read_recommendations = heredoc(function(){/*  <style>
+            .fw_most_read_recommendations {
+                background-color: #f5f6f8;
+                border-top: 4px solid #2a303b;
+                padding: 0 20px 20px 20px;
+            }
+            .fw_most_read_recommendations-articles-type {
+                font-family: "Marr Sans Condensed",DINCondensed-Bold,"Roboto Condensed","Arial Narrow",sans-serif;
+                font-size: 28px;
+                line-height: 1.4;
+                font-weight: 700;
+                margin: 0;
+                margin-top: 28px;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-articles {
+                margin: 0;
+                padding: 0;
+                background-color: hsl(220, 18%, 97%);
+                list-style: none;
+            }
+            .fw_most_read_recommendations-article:not(:last-child) {
+                border-bottom: 1px solid #d5d8dc;
+            }
+            .fw_most_read_recommendations-article a {
+                display: flex;
+                padding: 24px 0;
+                text-decoration: none;
+                color: #2a303b;
+            }
+            .fw_most_read_recommendations-article-number {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                margin-top: -3px;
+                display: flex;
+                font-weight: 600;
+                line-height: normal;
+                color: #2a303b;
+                font-size: 32px;
+            }
+            .fw_most_read_recommendations-article-title  {
+                font-family: "The Antiqua B",Georgia,Droid-serif,serif;
+                display: flex;
+                color: #2a303b;
+                font-size: 18px;
+                line-height: 1.3;
+                letter-spacing: -0.02px;
+                font-weight: 400;
+                padding-left: 16px;
+            }
+            .fw_most_read_recommendations-article a:hover .fw_most_read_recommendations-article-title {
+                text-decoration: underline;
+            }
+        </style>   <div class="fw_most_read_recommendations"> <p class="fw_most_read_recommendations-articles-type">  Les plus lus  </p> <ul class="fw_most_read_recommendations-articles">  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/live/2022/03/11/guerre-en-ukraine-2-5-millions-de-personnes-ont-fui-le-pays-depuis-l-invasion-russe-il-y-a-seize-jours_6117025_3210.html"> <span class="fw_most_read_recommendations-article-number">1</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine : Bruxelles propose 500 millions d’euros supplémentaires pour financer des armes</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/livres/article/2022/03/10/arpad-soltesz-nous-autres-slovaques-nous-savons-exactement-ce-que-cela-signifie-d-etre-liberes-par-nos-freres-slaves-russes_6116929_3260.html"> <span class="fw_most_read_recommendations-article-number">2</span>  <span class="fw_most_read_recommendations-article-title">Arpad Soltesz : « Nous autres Slovaques, nous savons exactement ce que cela signifie d’être libérés par nos frères slaves russes »</span>  </a> </li>  <li class="fw_most_read_recommendations-article"> <a href="https://www.lemonde.fr/international/article/2022/03/11/guerre-en-ukraine-la-russie-recrute-des-mercenaires-syriens_6117082_3210.html"> <span class="fw_most_read_recommendations-article-number">3</span>  <span class="fw_most_read_recommendations-article-title">Guerre en Ukraine : la Russie recrute des mercenaires syriens</span>  </a> </li>  </ul>  </div>  */}); 
